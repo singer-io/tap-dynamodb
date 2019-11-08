7613897157ff7f8aabc216290049f9d4aede089a
Collin Simon <csimon@talend.com>
2019-11-08 20:43:52 +0000

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-dynamodb",
+    version="0.0.0",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_dynamodb"],
+    install_requires=[
+        "singer-python==5.9.0",
+        'terminaltables==3.1.0',
+        'boto3==1.10.14',
+    ],
+    extras_require={
+        'dev': [
+            'ipdb',
+            'pylint',
+            'nose'
+        ]
+    },
+    entry_points="""
+    [console_scripts]
+    tap-dynamodb=tap_dynamodb:main
+    """,
+    packages=["tap_dynamodb"],
+    package_data = {},
+    include_package_data=True,
+)
