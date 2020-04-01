1c1e4312942e58bbec42a04ee16ef7f15af06e81
Kyle Allan <kyle@stitchdata.com>
2020-04-01 18:35:50 +0000

    bump to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-dynamodb",
-    version="0.4.3",
+    version="1.0.0",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
