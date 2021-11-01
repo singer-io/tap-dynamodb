a8d1c3c1166ac073f6aff564555a13a6dad21646
Dan Mosora <30501696+dmosorast@users.noreply.github.com>
2021-11-01 21:09:27 +0000

    Pylint changes to tests

--- tests/test_dynamodb_projections.py
+++ tests/test_dynamodb_projections.py
@@ -4,9 +4,9 @@ import singer
 from boto3.dynamodb.types import TypeSerializer
 
 from tap_tester.scenario import (SCENARIOS)
-import tap_tester.connections as connections
-import tap_tester.menagerie   as menagerie
-import tap_tester.runner      as runner
+from tap_tester import connections
+from tap_tester import menagerie
+from tap_tester import runner
 from base import TestDynamoDBBase
 
 LOGGER = singer.get_logger()
