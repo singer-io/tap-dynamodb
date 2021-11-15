14c019c60e160b814bfdb4918ae9c7b13c437e9f
harshpatel4_crest <harsh.patel4@crestdatasys.com>
2021-11-15 14:52:06 +0530

    added unittests

--- tests/unittests/test_deserializer.py
+++ tests/unittests/test_deserializer.py
@@ -17,6 +17,19 @@ class TestDeserializer(unittest.TestCase):
         # verify that we get empty list if the data is not found in the record
         self.assertEquals(output, {'Artist': 'No One You Know5', 'metadata': []})
 
+    def test_projection_expression_all_list_data_not_found_positive(self):
+        '''
+            Verify that we get empty list if the data is not found in the record
+            Example Projection: Artist, metadata[0]
+            Stream Record: {'Artist': 'No One You Know5', 'metadata': ['test']}
+        '''
+        mock_record = {'Artist': 'No One You Know5', 'metadata': ['test']}
+        mock_projections =  [['Artist'], ['metadata[0]']]
+
+        deserializer = deserialize.Deserializer()
+        output = deserializer.apply_projection(mock_record, mock_projections)
+        # verify that we get empty list if the data is not found in the record
+        self.assertEquals(output, {'Artist': 'No One You Know5', 'metadata': ['test']})
 
     def test_projection_expression_some_list_data_not_found(self):
         '''
@@ -32,6 +45,19 @@ class TestDeserializer(unittest.TestCase):
         # verify that we only get the available data in the list when user expect the data that is not available
         self.assertEquals(output, {'Artist': 'No One You Know5', 'metadata': ['test1']})
 
+    def test_projection_expression_some_list_data_not_found_positive(self):
+        '''
+            Verify that we only get the available data in the list when user expect the data that is not available
+            Example Projection: Artist, metadata[0], metadata[1]
+            Stream Record: {'Artist': 'No One You Know','metadata': ['test1', 'test2']}
+        '''
+        mock_record = {'Artist': 'No One You Know5','metadata': ['test1', 'test2']}
+        mock_projections =  [['Artist'], ['metadata[0]'], ['metadata[1]']]
+
+        deserializer = deserialize.Deserializer()
+        output = deserializer.apply_projection(mock_record, mock_projections)
+        # verify that we only get the available data in the list when user expect the data that is not available
+        self.assertEquals(output, {'Artist': 'No One You Know5', 'metadata': ['test1', 'test2']})
 
     def test_projection_expression_parent_child_data_list(self):
         '''
@@ -47,6 +73,20 @@ class TestDeserializer(unittest.TestCase):
         # verify that we get empty dict when the element in the list is parent element and it is not found
         self.assertEquals(output, {'Artist': 'No One You Know5', 'metadata': [{}]})
 
+    def test_projection_expression_parent_child_data_list_positive(self):
+        '''
+            Verify that we get empty dict when the element in the list is parent element and it is not found
+            Example Projection: Artist, metadata[0].Age
+            Stream Record: {'Artist': 'No One You Know5', 'metadata': [{'Age': 'Test'}]}
+        '''
+        mock_record = {'Artist': 'No One You Know5', 'metadata': [{'Age': 'Test'}]}
+        mock_projections =  [['Artist'], ['metadata[0]', 'Age']]
+
+        deserializer = deserialize.Deserializer()
+        output = deserializer.apply_projection(mock_record, mock_projections)
+        # verify that we get empty dict when the element in the list is parent element and it is not found
+        self.assertEquals(output, {'Artist': 'No One You Know5', 'metadata': [{'Age': 'Test'}]})
+
     def test_projection_expression_parent_child_data_dictionary(self):
         '''
             Veriy that we get None when the parent data is not found and we are requesting for child data
@@ -60,3 +100,17 @@ class TestDeserializer(unittest.TestCase):
         output = deserializer.apply_projection(mock_record, mock_projections)
         # veriy that we get None when the parent data is not found
         self.assertEquals(output, {'Artist': 'No One You Know5', 'metadata': {'inner_metadata': None}})
+
+    def test_projection_expression_parent_child_data_dictionary_positive(self):
+        '''
+            Veriy that we get None when the parent data is not found and we are requesting for child data
+            Example Projection: Artist, metadata.inner_metadata
+            Stream Record: {'Artist': 'No One You Know5', 'metadata': {'inner_metadata': 'Test'}}
+        '''
+        mock_record = {'Artist': 'No One You Know5', 'metadata': {'inner_metadata': 'Test'}}
+        mock_projections =  [['Artist'], ['metadata', 'inner_metadata']]
+
+        deserializer = deserialize.Deserializer()
+        output = deserializer.apply_projection(mock_record, mock_projections)
+        # veriy that we get None when the parent data is not found
+        self.assertEquals(output, {'Artist': 'No One You Know5', 'metadata': {'inner_metadata': 'Test'}})
