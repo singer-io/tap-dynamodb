eae390335f4d91ebcbfc186e8997693eb88d0565
namrata270998 <namrata.brahmbhatt@crestdatasys.com>
2021-12-07 12:23:42 +0530

    added in log_based test and comments

--- tests/test_dynamodb_log_based_projections.py
+++ tests/test_dynamodb_log_based_projections.py
@@ -22,8 +22,8 @@ class DynamoDBLogBasedProjections(TestDynamoDBBase):
                 'HashType': 'N',
                 'generator': self.generate_items,
                 'num_rows': 100,
-                # Added two extra reserve word field `Absolute`, `Comment` to verify expression attributes
-                'ProjectionExpression': 'Absolute, Comment, int_id, string_field, decimal_field, int_list_field[1], map_field.map_entry_1, string_list[2], map_field.list_entry[2], list_map[1].a',
+                # Added two extra reserve word field `Absolute`, `Comment` and `Name[1].Comment` to verify expression attributes
+                'ProjectionExpression': 'Name[1].Comment,Absolute, Comment, int_id, string_field, decimal_field, int_list_field[1], map_field.map_entry_1, string_list[2], map_field.list_entry[2], list_map[1].a',
                 'top_level_keys': {'Absolute', 'Comment', 'int_id', 'string_field', 'decimal_field', 'int_list_field', 'map_field', 'string_list', 'list_map'},
                 'top_level_list_keys': {'int_list_field', 'string_list', 'list_map'},
                 'nested_map_keys': {'map_field': {'map_entry_1', 'list_entry'}},
@@ -35,6 +35,7 @@ class DynamoDBLogBasedProjections(TestDynamoDBBase):
         for i in range(start_key, start_key + num_items):
             record = {
                 'Comment': 'Talend stitch',
+                'Name': ['name1', {'Comment': "Test_comment"}],
                 'Absolute': 'true',
                 'int_id': i,
                 'decimal_field': decimal.Decimal(str(i) + '.00000000001'),
@@ -76,7 +77,7 @@ class DynamoDBLogBasedProjections(TestDynamoDBBase):
                     "breadcrumb": [],
                     "metadata": {
                         'replication-method': 'LOG_BASED',
-                        'tap-mongodb.expression': 'Comment, Absolute', # `expression` field for reserve word.
+                        'tap-mongodb.expression': 'Comment, Absolute, Name[1].Comment', # `expression` field for reserve word.
                         'tap-mongodb.projection': table_configs[0]['ProjectionExpression']
                     }
                 }
