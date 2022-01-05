a643e03ad070d9e68ca99c11492016e6a60fc986
prijendev <prijen.khokhani@crestdatasys.com>
2022-01-05 15:09:34 +0530

    Resolved review comments

--- tests/test_dynamodb_log_based_projections.py
+++ tests/test_dynamodb_log_based_projections.py
@@ -23,9 +23,9 @@ class DynamoDBLogBasedProjections(TestDynamoDBBase):
                 'generator': self.generate_items,
                 'num_rows': 100,
                 # Added three extra reserve word fields `Absolute`, `Comment` and `Name[1].Comment` to verify expression attributes
-                'ProjectionExpression': 'Name[1].Comment,Absolute, Comment, int_id, string_field, decimal_field, int_list_field[1], map_field.map_entry_1, string_list[2], map_field.list_entry[2], list_map[1].a',
-                'top_level_keys': {'Absolute', 'Comment', 'int_id', 'string_field', 'decimal_field', 'int_list_field', 'map_field', 'string_list', 'list_map'},
-                'top_level_list_keys': {'int_list_field', 'string_list', 'list_map'},
+                'ProjectionExpression': 'Name[1].Comment, Absolute, Comment, int_id, string_field, decimal_field, int_list_field[1], map_field.map_entry_1, string_list[2], map_field.list_entry[2], list_map[1].a',
+                'top_level_keys': {'Name', 'Absolute', 'Comment', 'int_id', 'string_field', 'decimal_field', 'int_list_field', 'map_field', 'string_list', 'list_map'},
+                'top_level_list_keys': {'int_list_field', 'string_list', 'list_map', 'Name'},
                 'nested_map_keys': {'map_field': {'map_entry_1', 'list_entry'}},
             }
         ]
