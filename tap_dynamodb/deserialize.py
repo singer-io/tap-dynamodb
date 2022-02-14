3d49bbfb034b16253eefb72c8305df72e2b5b658
Harsh <80324346+harshpatel4crest@users.noreply.github.com>
2022-02-14 11:08:44 +0530

    TDL-13692: Missing parent objects from projection expression causes key error and TDL-16140: Fix key error and index error while applying projection expression (#35)
    * added code change for handling key error and index error
    
    * resolve pylint error
    
    * updated the unittests
    
    * updated config.yml to run unittest to run on CCi
    
    * added unittests
    
    * updated the code to handle child dict data
    
    * added comments with example for projection
    
    * updated config.yml file and resolved PR review comments
    
    * resolve slack failure error in config file
    
    * updated version 2 to 2.1
    
    * remoed SCENARIO from the test cases
    
    * resolved comments
    
    * reverted changes
    
    * added tap-tester-venv, tap_tester_sandbox and check for verifying that we replicated records
    
    Co-authored-by: namrata270998 <namrata.brahmbhatt@crestdatasys.com>

--- tap_dynamodb/deserialize.py
+++ tap_dynamodb/deserialize.py
@@ -63,23 +63,31 @@ class Deserializer(TypeDeserializer):
                 breadcrumb_key = breadcrumb[0].split('[')[0]
                 index = int(breadcrumb[0].split('[')[1].split(']')[0])
                 if output.get(breadcrumb_key):
-                    output[breadcrumb_key].append(record[breadcrumb_key][index])
+                    # only prepare output if the list field contains data at that index position in record
+                    if len(record.get(breadcrumb_key)) > index:
+                        output[breadcrumb_key].append(record[breadcrumb_key][index])
                 else:
-                    output[breadcrumb_key] = [record[breadcrumb_key][index]]
-
+                    output[breadcrumb_key] = []
+                    # only prepare output if the list field contains data at that index position in record
+                    if record.get(breadcrumb_key) and len(record.get(breadcrumb_key)) > index:
+                        output[breadcrumb_key].append(record[breadcrumb_key][index])
             else:
                 output[breadcrumb[0]] = record.get(breadcrumb[0])
         else:
             if '[' in breadcrumb[0]:
                 breadcrumb_key = breadcrumb[0].split('[')[0]
                 index = int(breadcrumb[0].split('[')[1].split(']')[0])
-                if output.get(breadcrumb_key) is None:
+                if not output.get(breadcrumb_key):
                     output[breadcrumb_key] = [{}]
-                self._apply_projection(record[breadcrumb_key][index], breadcrumb[1:], output[breadcrumb_key][0])
+                # only prepare output if the list field contains data at that index position in record
+                if record.get(breadcrumb_key) and len(record.get(breadcrumb_key)) > index:
+                    self._apply_projection(record[breadcrumb_key][index], breadcrumb[1:], output[breadcrumb_key][0])
             else:
                 if output.get(breadcrumb[0]) is None:
                     output[breadcrumb[0]] = {}
-                self._apply_projection(record[breadcrumb[0]], breadcrumb[1:], output[breadcrumb[0]])
+                # keep empty dict if the data is not found in the record
+                if record.get(breadcrumb[0]):
+                    self._apply_projection(record.get(breadcrumb[0], {}), breadcrumb[1:], output[breadcrumb[0]])
 
     def apply_projection(self, record, projections):
         output = {}
