b14c1a63d29503baef90dec747ce2398ad9b9505
Leslie VanDeMark <lvandemark@talend.com>
2024-01-24 14:48:51 +0000

    Updates for python 3.11.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-dynamodb",
-    version="1.2.3",
+    version="1.3.0",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
@@ -11,15 +11,15 @@ setup(
     py_modules=["tap_dynamodb"],
     install_requires=[
         'boto3==1.14.9',
-        "singer-python==5.9.0",
+        "singer-python==6.0.0",
         'terminaltables==3.1.0',
-        'backoff==1.8.0',
+        'backoff==2.2.1',
     ],
     extras_require={
         'dev': [
             'ipdb',
             'pylint',
-            'nose'
+            'nose2'
         ]
     },
     entry_points="""
