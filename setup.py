3f7b9ce0c83f2d54efc00d8e1444f87b41cadaaa
Leslie VanDeMark <38043390+leslievandemark@users.noreply.github.com>
2024-12-19 10:37:29 -0500

    removes terminaltables (#60)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-dynamodb",
-    version="1.3.0",
+    version="1.3.1",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
@@ -12,7 +12,6 @@ setup(
     install_requires=[
         'boto3==1.14.9',
         "singer-python==6.0.0",
-        'terminaltables==3.1.0',
         'backoff==2.2.1',
     ],
     extras_require={
